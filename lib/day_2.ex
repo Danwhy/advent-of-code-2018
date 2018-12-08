8c3a587f9cd786ecce0a8de9f115fa43f1b40b6a
Danielwhyte <danwhy@gmail.com>
2018-12-08 14:54:38 +0000

    adds solution for day 2, part 2

--- lib/day_2.ex
+++ lib/day_2.ex
@@ -6,11 +6,22 @@ defmodule Advent.Day2 do
 
     case part do
       1 -> part_1(input)
+      2 -> part_2(input)
     end
   end
 
   def part_1(input) do
     count_chars(input, 2) * count_chars(input, 3)
+  end
+
+  def part_2(input) do
+    case find_similar(input) do
+      {str_1, _str_2, i} ->
+        String.slice(str_1, 0..(i - 1)) <> String.slice(str_1, (i + 1)..String.length(str_1))
+
+      nil ->
+        nil
+    end
   end
 
   def count_chars(input, count) do
@@ -26,5 +37,29 @@ defmodule Advent.Day2 do
     list
     |> (fn l -> Enum.group_by(l, fn e -> Enum.count(l, &(e == &1)) end) end).()
     |> Map.has_key?(count)
+  end
+
+  def difference(str_1, str_2) do
+    str_1
+    |> String.split("", trim: true)
+    |> Enum.with_index()
+    |> Enum.filter(fn {c, i} -> String.at(str_2, i) != c end)
+  end
+
+  def find_similar([]), do: nil
+
+  def find_similar([head | tail]) do
+    case Enum.reduce_while(tail, nil, fn s, acc ->
+           case difference(head, s) do
+             diff when is_list(diff) and length(diff) == 1 ->
+               {:halt, {head, s, diff |> List.first() |> elem(1)}}
+
+             _ ->
+               {:cont, acc}
+           end
+         end) do
+      nil -> find_similar(tail)
+      similar -> similar
+    end
   end
 end
--- test/day_2_test.exs
+++ test/day_2_test.exs
@@ -1,7 +1,7 @@
 defmodule AdventTest.Day2Test do
   use ExUnit.Case
 
-  describe "Day 2" do
+  describe "Day 2 - part 1" do
     test "has element n times?" do
       assert Advent.Day2.has_element_n_times?(["a", "b", "b", "c", "d", "e"], 2)
       refute Advent.Day2.has_element_n_times?(["a", "b", "b", "c", "d", "e"], 3)
@@ -38,6 +38,55 @@ defmodule AdventTest.Day2Test do
                "dvded",
                "dffdf"
              ]) == 6
+    end
+  end
+
+  describe "Day 2 - part 2" do
+    test "difference" do
+      assert Advent.Day2.difference("abc", "abc") == []
+      assert Advent.Day2.difference("abd", "abc") == [{"d", 2}]
+
+      assert Advent.Day2.difference("qwert", "asdfg") == [
+               {"q", 0},
+               {"w", 1},
+               {"e", 2},
+               {"r", 3},
+               {"t", 4}
+             ]
+    end
+
+    test "find similar" do
+      assert Advent.Day2.find_similar(["abc", "abd"]) == {"abc", "abd", 2}
+      assert Advent.Day2.find_similar(["adc", "abd"]) == nil
+      assert Advent.Day2.find_similar(["hudkfh", "hudkfb", "hudfff"]) == {"hudkfh", "hudkfb", 5}
+      assert Advent.Day2.find_similar(["hudffd", "saduhi", "dsbhdf"]) == nil
+
+      assert Advent.Day2.find_similar([
+               "abcde",
+               "fghij",
+               "klmno",
+               "pqrst",
+               "fguij",
+               "axcye",
+               "wvxyz"
+             ]) == {"fghij", "fguij", 2}
+    end
+
+    test "part 2" do
+      assert Advent.Day2.part_2(["abc", "abd"]) == "ab"
+      assert Advent.Day2.part_2(["adc", "abd"]) == nil
+      assert Advent.Day2.part_2(["hudkfh", "hudkfb", "hudfff"]) == "hudkf"
+      assert Advent.Day2.part_2(["hudffd", "saduhi", "dsbhdf"]) == nil
+
+      assert Advent.Day2.part_2([
+               "abcde",
+               "fghij",
+               "klmno",
+               "pqrst",
+               "fguij",
+               "axcye",
+               "wvxyz"
+             ]) == "fgij"
     end
   end
 end
