f015f019bd120f559e447d43107a87290d8c1e8f
Danielwhyte <danwhy@gmail.com>
2018-12-09 12:34:33 +0000

    adds test cases for actual solutions

--- test/advent_test.exs
+++ test/advent_test.exs
@@ -2,13 +2,29 @@ defmodule AdventTest do
   use ExUnit.Case
   doctest Advent
 
+  @solutions 1..2
+
   test "Advent module runs each day" do
-    assert Advent.run(:day_1)
-    assert Advent.run(:day_1, 2)
+    @solutions
+    |> Enum.each(fn s ->
+      day = String.to_existing_atom("day_" <> to_string(s))
 
-    assert Advent.run(:day_2)
-    assert Advent.run(:day_2, 2)
+      assert Advent.run(day)
+      assert Advent.run(day, 2)
+    end)
+  end
 
+  test "Solution Day 1" do
+    assert Advent.run(:day_1) == 416
+    assert Advent.run(:day_1, 2) == 56752
+  end
+
+  test "Solution Day 2" do
+    assert Advent.run(:day_2) == 6723
+    assert Advent.run(:day_2, 2) == "prtkqyluiusocwvaezjmhmfgx"
+  end
+
+  test "Solution Day 3" do
     assert Advent.run(:day_3) == 104_126
   end
 end
